02423a8a40915ad9843c95ef79888cf928c2d2b9
oarthurfc <arthuryouface@gmail.com>
2024-07-31 18:31:34 -0300

    Commit inicial

---
+++ src/main/java/com/oarthurfc/PetShopDTI/PetShopDtiApplication.java
@@ -0,0 +1,13 @@
+package com.oarthurfc.PetShopDTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopDtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopDtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oarthurfc/PetShopDTI/PetShopDtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oarthurfc.PetShopDTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopDtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
