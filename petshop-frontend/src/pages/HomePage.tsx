b9ae7195a9b327222d4f420e2a5747ec944e0023
oarthurfc <arthuryouface@gmail.com>
2024-08-02 00:08:00 -0300

    feat: Implementando Day Picker

--- petshop-frontend/src/pages/HomePage.tsx
+++ petshop-frontend/src/pages/HomePage.tsx
@@ -1,24 +1,42 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 import '../App.css';
 import '../services/api'
-import { Calendar, PawPrint, Search } from 'lucide-react';
+import { Calendar, PawPrint, Search, X } from 'lucide-react';
 
 const HomePage: React.FC = () => {
-  const [data, setData] = useState('');
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>();
+  const [isModalDataOpen, setIsModalData] = useState(false);
   const [qtdPequenos, setQtdPequenos] = useState(0);
   const [qtdGrandes, setQtdGrandes] = useState(0);
   const [result, setResult] = useState<{ nome: string; precoTotal: number } | null>(null);
 
+  function openModalData(){
+    setIsModalData(true)
+  }
+
+  function closeModalData(){
+    setIsModalData(false)
+  }
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
+    if (!selectedDate) {
+      console.error('Por favor, selecione uma data.');
+      return;
+    }
+
+    const formattedDate = selectedDate.toLocaleDateString('pt-BR');
+
     try {
       const response = await axios.get('http://127.0.0.1:8080/api/petshops/melhor-petshop', {
-        params: { data, qtdPequenos, qtdGrandes }
+        params: { data: formattedDate, qtdPequenos, qtdGrandes }
       });
       setResult(response.data);
     } catch (error) {
-      console.error('Erro ao encontrar PetShop!', error);
+      console.error('There was an error fetching the pet shop!', error);
     }
   };
 
@@ -26,16 +44,15 @@ const HomePage: React.FC = () => {
     <div className="home-page">
       <h1>Encontre o <br/>Melhor PetShop</h1>
       <form className="form-container" onSubmit={handleSubmit}>
-        <div className="box-input">
+        <div className="box-input" id="input-data">
           <p>Data</p>
-          <div className="input-content">
+          <div onClick={openModalData} className="input-content" id="abrir-modal">
             <Calendar className="input-icon"/>
-            <input
-              type="text"
-              placeholder="Data (dd/mm/yyyy)"
-              value={data}
-              onChange={(e) => setData(e.target.value)}
-            />
+            {selectedDate ?(
+              <p>{selectedDate.toLocaleDateString('pt-BR')}</p>
+            ) : 
+              <p>Selecione uma data</p>
+            }
           </div>
         </div>
         <div className="box-num-caes">
@@ -75,6 +92,23 @@ const HomePage: React.FC = () => {
           <p><b>Preço Total: R${result.precoTotal}</b></p>
         </div>
       )}
+
+
+      {isModalDataOpen && (
+        <div className="modal-backdrop">
+          <div className="modal-data">
+            <X onClick={closeModalData} className="modal-close"/>
+            <DayPicker
+                mode="single"
+                selected={selectedDate}
+                onSelect={setSelectedDate}
+                footer={selectedDate ? `Data selecionada: ${selectedDate.toLocaleDateString('pt-BR')}` : 'Please pick a date.'}
+            />
+          </div>
+        </div>
+      )}
+
+
     </div>
   );
 };
